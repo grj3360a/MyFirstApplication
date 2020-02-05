f50502d9959b7db183fa2cf9af0f5a08a45ea592
r-info-p222 <r-info-p222@iut-tlse3.fr>
2020-02-05 09:27:36 +0100

    On a rajouter plein de truc chouette

--- app/src/main/java/extension/domaine/myfirstapplication/MainActivity.java
+++ app/src/main/java/extension/domaine/myfirstapplication/MainActivity.java
@@ -2,13 +2,52 @@ package extension.domaine.myfirstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText login = findViewById(R.id.loginText);
+                EditText password = findViewById(R.id.passwordText);
+
+                if(login.getText().toString().toString().trim().length() == 0){
+                    Toast.makeText(getApplicationContext(), "Pas de login spécifié", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(password.getText().toString().trim().length() == 0){
+                    Toast.makeText(getApplicationContext(), "Pas de mot de passe spécifié", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(!login.getText().toString().equals("grj3360a")){
+                    Toast.makeText(getApplicationContext(), "Login invalide", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(!password.getText().toString().equals("mdp")){
+                    Toast.makeText(getApplicationContext(), "Password invalide", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Intent in = new Intent(MainActivity.this, MenuPrincipal.class);
+                startActivity(in);
+            }
+        });
     }
 }
